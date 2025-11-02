fd13e7134f959f4cbbaebb70c5a4093b5e67ed15
Rasel Ahmed <gsmrashidul24@gmail.com>
2025-11-02 20:11:10 +0300

    Update omni_a12s.mk

--- omni_a12s.mk
+++ omni_a12s.mk
@@ -6,17 +6,19 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from a12s device
 $(call inherit-product, device/samsung/a12s/device.mk)
 
 PRODUCT_DEVICE := a12s
-PRODUCT_NAME := omni_a12s
+PRODUCT_NAME := twrp_a12s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A127F
 PRODUCT_MANUFACTURER := samsung
